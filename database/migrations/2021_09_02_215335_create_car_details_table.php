111d68dda09bdffc48beefed2cbe5674e032149d
Yll Latifi <latifiyll@gmail.com>
2021-09-03 00:43:52 +0200

    migration and seeds

---
+++ database/migrations/2021_09_02_215335_create_car_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('car_id');
+            $table->double('bought',15,2);
+            $table->double('other_expenses',15,2);
+            $table->double('sold',15,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_details');
+    }
+}
